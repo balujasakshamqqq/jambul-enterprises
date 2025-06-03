d853d7a2117a0579641e3319ef5ff570c330b1d7
Saksham Baluja <personal@Sakshams-MacBook-Air.local>
2025-06-03 06:12:31 +0530

    Switched to Next.js Vercel version

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "100px" }}>
+      <h1>Welcome to Jambul Enterprises</h1>
+      <p>This site is now live on Vercel ✅</p>
+    </div>
+  );
+}
